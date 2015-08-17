90ee0ddaf23e2b5ac3f17bc06a03b54996a2279e
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2015-08-17 11:43:43 +0200

    Fatals, min stacktrace level, version and some cosmetics.

--- lib/kantox/herro/log.rb
+++ lib/kantox/herro/log.rb
@@ -8,6 +8,18 @@ module Kantox
 
       MULTILINE = !(FalseClass === Kantox::Herro.config.log!.multiline)
 
+      def self.severity_int severity
+        case severity
+        when 'debug', 'DEBUG', '0', 0 then 0
+        when 'info', 'INFO', '1', 1 then 1
+        when 'warn', 'WARN', '2', 2 then 2
+        when 'error', 'ERROR', '3', 3 then 3
+        when 'fatal', 'FATAL', '4', 4 then 4
+        else 2
+        end
+      end
+      MIN_BACKTRACE_LEVEL = severity_int Kantox::Herro.config.log!.min_backtrace_level
+
       NESTED_OFFSET = Kantox::Herro.config.log!.nested || 30
       TERMINAL_WIDTH = Kantox::Herro.config.log!.terminal || ($stdin.winsize.last rescue 80) - NESTED_OFFSET
       BACKTRACE_LENGTH = Kantox::Herro.config.log!.backtrace!.len || 10
@@ -143,7 +155,10 @@ module Kantox
         case what
         when Exception then log_exception what, severity, datetime, skip
         when Array then what.map { |what| log_with_trace what, severity, datetime, skip }.join($/)
-        else log_string preen_string(what.to_s), severity, datetime
+        else
+          Log.severity_int(severity) > MIN_BACKTRACE_LEVEL ?
+            log_string(preen_string(what.to_s), severity, datetime) :
+            log_with_trace(what, severity, datetime, skip)
         end
       end
 
